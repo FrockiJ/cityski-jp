204dd2d7b74701e4f370257a799b145ad74b8603
Hank <whtu.ncku.develop@gmail.com>
2025-12-11 13:58:03 +0800

    刪除EcpayTestController

--- apps/server/src/ecpay/ecpay.module.ts
+++ apps/server/src/ecpay/ecpay.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { EcpayService } from './ecpay.service';
 import { EcpayController } from './ecpay.controller';
-import { EcpayTestController } from './ecpay-test.controller';
 import { EcpayConfig } from './ecpay.config';
 import { EcpayCryptoService } from './ecpay-crypto.service';
 import { OrdersModule } from 'src/orders/orders.module';
@@ -9,7 +8,7 @@ import { OrdersModule } from 'src/orders/orders.module';
 @Module({
   imports: [OrdersModule],
   providers: [EcpayService, EcpayConfig, EcpayCryptoService],
-  controllers: [EcpayController, EcpayTestController],
+  controllers: [EcpayController],
   exports: [EcpayService, EcpayConfig, EcpayCryptoService],
 })
 export class EcpayModule {}

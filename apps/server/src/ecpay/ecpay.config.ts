e0172123b22a5d0be6230d746b7c8f2f81bbb6af
simonwei <simonwei@linkervision.com>
2025-12-16 09:33:20 +0800

    refactor: Simplify EcpayConfig by removing unused variables and streamlining environment setup

--- apps/server/src/ecpay/ecpay.config.ts
+++ apps/server/src/ecpay/ecpay.config.ts
@@ -9,46 +9,18 @@ export class EcpayConfig {
   readonly apiUrlStage: string;
   readonly apiUrlProd: string;
   readonly environment: string;
-  readonly websiteEnvironment: string;
   readonly returnUrl: string;
   readonly orderResultUrl: string;
-  readonly clientBackUrl: string;
   readonly clientDomain: string;
 
   constructor(private configService: ConfigService) {
-    // 取得 ECPay 環境設定 (控制使用哪個商家帳號和 API)
     this.environment = this.configService.get<string>('ECPAY_ENVIRONMENT', 'stage');
-
-    // 取得網站環境設定 (控制使用哪個域名)
-    this.websiteEnvironment = this.configService.get<string>('WEBSITE_ENVIRONMENT', 'local');
-
-    // 根據 ECPAY_ENVIRONMENT 切換帳號資訊
-    if (this.environment === 'prod') {
-      // 正式版帳號
-      this.merchantId = this.configService.get<string>('ECPAY_MERCHANT_ID_PROD');
-      this.hashKeyPayment = this.configService.get<string>('ECPAY_HASH_KEY_PAYMENT_PROD');
-      this.hashIvPayment = this.configService.get<string>('ECPAY_HASH_IV_PAYMENT_PROD');
-    } else {
-      // 測試版帳號
-      this.merchantId = this.configService.get<string>('ECPAY_MERCHANT_ID_STAGE');
-      this.hashKeyPayment = this.configService.get<string>('ECPAY_HASH_KEY_PAYMENT_STAGE');
-      this.hashIvPayment = this.configService.get<string>('ECPAY_HASH_IV_PAYMENT_STAGE');
-    }
-
-    this.apiUrlStage = this.configService.get<string>(
-      'ECPAY_API_URL_STAGE',
-    );
-    this.apiUrlProd = this.configService.get<string>(
-      'ECPAY_API_URL_PROD',
-    );
-
-    // 根據 WEBSITE_ENVIRONMENT 切換域名
-    if (this.websiteEnvironment === 'uat') {
-      this.clientDomain = this.configService.get<string>('ECPAY_CLIENT_DOMAIN_UAT');
-    } else {
-      // local 或其他預設使用 local 域名
-      this.clientDomain = this.configService.get<string>('ECPAY_CLIENT_DOMAIN_LOCAL');
-    }
+    this.merchantId = this.configService.get<string>('ECPAY_MERCHANT_ID');
+    this.hashKeyPayment = this.configService.get<string>('ECPAY_HASH_KEY_PAYMENT');
+    this.hashIvPayment = this.configService.get<string>('ECPAY_HASH_IV_PAYMENT');
+    this.apiUrlStage = this.configService.get<string>('ECPAY_API_URL_STAGE');
+    this.apiUrlProd = this.configService.get<string>('ECPAY_API_URL_PROD');
+    this.clientDomain = this.configService.get<string>('ECPAY_CLIENT_DOMAIN');
 
     this.returnUrl = this.configService.get<string>(
       'ECPAY_RETURN_URL',

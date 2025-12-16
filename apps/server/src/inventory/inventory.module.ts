f10aa0fbb7347fd11616e5300f9bdeff74ce94f8
simonwei <simonwei@linkervision.com>
2025-12-16 09:50:25 +0800

    fix: Add User entity to InventoryModule imports for proper dependency injection

--- apps/server/src/inventory/inventory.module.ts
+++ apps/server/src/inventory/inventory.module.ts
@@ -5,9 +5,10 @@ import { InventoryService } from './inventory.service';
 import { Order } from 'src/orders/entities/order.entity';
 import { OrderReservation } from 'src/order-reservations/entities/order-reservation.entity';
 import { Reservation } from 'src/reservations/entities/reservation.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderReservation, Reservation])],
+  imports: [TypeOrmModule.forFeature([Order, OrderReservation, Reservation, User])],
   controllers: [InventoryController],
   providers: [InventoryService],
   exports: [InventoryService],
